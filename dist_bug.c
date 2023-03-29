1b14c55662400b70574683612009158207314ee4
Joel Smith <joelsmith2004@gmail.com>
2023-03-29 11:51:00 +0000

    29 Mar 2023, 19:51

--- dist_bug.c
+++ dist_bug.c
@@ -53,7 +53,7 @@ bool dist_bug(x, y){
         int angle = getRelativeGoalLocationAng(currentXPosition, currentYPosition, x, y);
         int angDif = angle - currentAngle;
         if (5<angDif || -5>angDif){
-             VWTurn(angle, 60);
+             VWTurn(angDif, 60);
              VWWait();
              VWGetPosition(&currentXPosition, &currentYPosition, &currentAngle);
 
