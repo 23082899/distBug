eaf80aa4de00453e53454d063dca279ed6aa8b98
Joel Smith <joelsmith2004@gmail.com>
2023-04-04 04:30:10 +0000

    4 Apr 2023, 12:30

--- dist_bug.c
+++ dist_bug.c
@@ -21,7 +21,7 @@ double getRelativeGoalLocationDis(int curX, int curY, int x, int y){
 bool checkAtPoint(double x, double y, int goalX, int goalY){
     float nx = goalX-x;
     float ny = goalY-y;
-    if ((nx >=-400 && nx <= 400)&&(ny >=-400 && ny <= 400)){
+    if ((nx >=-200 && nx <= 200)&&(ny >=-200 && ny <= 200)){
         return true;
     } else{
         return false;
